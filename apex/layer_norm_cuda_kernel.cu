1d7a4a5990e50a2e5ca2cf81dfd36691c5acd5a1
BBuf <1182563586@qq.com>
2024-01-10 20:59:13 +0800

    add more explain for apex layernorm

--- apex/layer_norm_cuda_kernel.cu
+++ apex/layer_norm_cuda_kernel.cu
@@ -232,6 +232,7 @@ void cuWelfordMuSigma2(
 }
 
 // 这个函数是上面的 cuWelfordMuSigma2 的Half特化，就不重复解析
+// 需要注意的是welford计算的compute type是float，所以这里存在精度转换
 template<> __device__
 void cuWelfordMuSigma2(
   const at::Half* __restrict__ vals,
@@ -358,9 +359,11 @@ void cuWelfordMuSigma2(
   }
 }
 
+// 计算倒数平方根
 template<typename U> U rsqrt(U v) {
   return U(1) / sqrt(v);
 }
+// 针对float参数的特化版本，使用了标准库函数
 template<> float rsqrt(float v) {
   return rsqrtf(v);
 }
@@ -368,6 +371,9 @@ template<> double rsqrt(double v) {
   return rsqrt(v);
 }
 
+//  这段代码定义了一个名为 SharedMemory 的模板结构体，它用于在 CUDA 设备函数中访问共享内存。
+// 共享内存是 CUDA 编程中的一种高效的内存类型，通常用于在一个 CUDA 块中的不同线程之间共享数据。
+// 代码包含了 SharedMemory 结构体的特化版本，专门用于 float 和 double 类型。
 namespace {
 // This is the un-specialized struct.  Note that we prevent instantiation of this
 // struct by putting an undefined symbol in the function body so it won't compile.
@@ -386,16 +392,21 @@ namespace {
 template <typename T>
 struct SharedMemory;
 
+// 这是 SharedMemory 结构体针对 float 类型的特化版本。
 template <>
 struct SharedMemory <float>
 {
+    // 这个函数返回一个指向共享内存的 float 类型指针。
     __device__ float *getPointer()
-    {
+    { 
+        // 这里声明了一个外部的共享内存数组 s_float，用于存储 float 类型的数据。
+        // extern 和 __shared__ 关键字指出这个数组是在共享内存中定义的。
         extern __shared__ float s_float[];
         return s_float;
     }
 };
 
+// 类似上面做了一个double类型的特化
 template <>
 struct SharedMemory <double>
 {
@@ -407,6 +418,12 @@ struct SharedMemory <double>
 };
 }
 
+// 这段代码定义了一个名为 cuApplyLayerNorm_ 的 CUDA 设备函数，用于计算LayerNorm（Layer Normalization）。
+// 定义了三种类型的模板参数。T 是输入值的类型，U 是中间计算（如均值和方差）的类型，而 V 是输出值的类型。
+// output_vals, mean, invvar, vals, gamma, beta 是指向不同数据的指针。
+// 在 LayerNorm 中，通常将一个张量分为两部分：一部分进行标准化处理，另一部分则不受影响。n1 和 n2 分别代表这两部分的大小。
+// 例如，如果你有一个形状为 [batch_size, channels, height, width] 的 4D 张量，并且你只想对最后两个维度进行 LayerNorm，
+// 那么 n1 将是 batch_size * channels，而 n2 则是 height * width。
 template<typename T, typename U, typename V> __device__
 void cuApplyLayerNorm_(
   V* __restrict__ output_vals,
@@ -425,17 +442,25 @@ void cuApplyLayerNorm_(
   // 1) blockDim.x == warpSize
   // 2) Tensors are contiguous
   //
+  // 这段代码遍历 n1 维度，每次处理一个索引 i1。
+  // 它假设每个 CUDA 块的线程维度 x 等于 warp 的大小，且张量在内存中是连续的。
+  // 一个线程可能处理很多行，所以这里的step取gridDim.y
   for (auto i1=blockIdx.y; i1 < n1; i1 += gridDim.y) {
     SharedMemory<U> shared;
-    U* buf = shared.getPointer();
-    U mu,sigma2;
+    U* buf = shared.getPointer(); // 创建一个 SharedMemory 实例用于处理类型 U 的数据。
+    U mu,sigma2; // 获取指向共享内存的指针。
+    // 调用 cuWelfordMuSigma2 函数计算给定索引 i1 处的均值（mu）和方差（sigma2）。
     cuWelfordMuSigma2(vals,n1,n2,i1,mu,sigma2,buf,rms_only);
 
+    // 定位到当前 i1 索引处的输入和输出的起始位置。
     const T* lvals = vals + i1*n2;
     V* ovals = output_vals + i1*n2;
+    // 计算逆方差 c_invvar。
     U c_invvar = rsqrt(sigma2 + epsilon);
+    // 计算每个 CUDA 块的线程总数 (numx) 和当前线程的一维索引 (thrx)。
     const int numx = blockDim.x * blockDim.y;
     const int thrx = threadIdx.x + threadIdx.y * blockDim.x;
+    // 如果提供了 gamma 和 beta 或者只计算 RMS，按照一定的规则应用它们来计算输出。
     if (gamma != NULL && (beta != NULL || rms_only)) {
       for (int i = thrx;  i < n2;  i+=numx) {
         U curr = static_cast<U>(lvals[i]);
@@ -446,7 +471,9 @@ void cuApplyLayerNorm_(
         }
 
       }
-    } else {
+    } 
+    // 否则，直接根据计算的均值和逆方差计算归一化值。
+    else {
       for (int i = thrx;  i < n2;  i+=numx) {
         U curr = static_cast<U>(lvals[i]);
         if (!rms_only) {
@@ -456,16 +483,19 @@ void cuApplyLayerNorm_(
         }
       }
     }
+    // 在每个 CUDA 块中，仅由一个线程（线程 (0,0)）更新均值和逆方差。
     if (threadIdx.x == 0 && threadIdx.y == 0) {
       if (!rms_only) {
         mean[i1] = mu;
       }
       invvar[i1] = c_invvar;
     }
+    // 用于同步块内的所有线程。
     __syncthreads();
   }
 }
 
+// 对上个函数的参数透传，不过rms_only设为False
 template<typename T, typename U, typename V=T> __global__
 void cuApplyLayerNorm(
   V* __restrict__ output_vals,
@@ -482,6 +512,8 @@ void cuApplyLayerNorm(
   cuApplyLayerNorm_<T, U, V>(output_vals, mean, invvar, vals, n1, n2, epsilon, gamma, beta, false);
 }
 
+// 这段代码定义了一个名为 clamp_by_magnitude 的 CUDA 设备函数模板，
+// 用于将给定值 curr_gamma 的绝对值限制在一个最小阈值 eps 以上
 template<typename V> __device__
 V clamp_by_magnitude(V curr_gamma, double eps)
 {
@@ -501,7 +533,13 @@ V clamp_by_magnitude(V curr_gamma, double eps)
   }
 }
 
-
+// 这段代码定义了一个名为 cuLoadWriteStridedInputs 的 CUDA 设备函数模板，用于在计算LayerNorm的梯度时，
+// 从输入张量中加载数据并进行必要的计算，将结果存储在 warp 缓冲区中。这个函数支持内存高效模式（MemoryEfficient）。
+// 模板参数 T, U, V 代表不同的数据类型。
+// bool MemoryEfficient 用于选择是否采用内存高效的方式处理数据。
+// __device__ 表明这是一个 CUDA 设备函数。
+// 函数参数包括各种用于LayerNorm梯度计算的数据，
+// 如输入/输出张量、梯度张量 dout、均值 mean、逆方差 invvar、缩放参数 gamma、偏移参数 beta 等。
 template<typename T, typename U, typename V, bool MemoryEfficient> __device__
 void cuLoadWriteStridedInputs(
     const int i1_block,
@@ -523,15 +561,20 @@ void cuLoadWriteStridedInputs(
     bool rms_only
     )
 {
+  // 计算 i1，表示当前处理的行索引。
   int i1 = i1_block+thr_load_row_off;
   if (i1 < i1_end) {
     for (int k = 0;  k < blockDim.y;  ++k) {
+      // 计算列索引 i2 和用于加载和写入数据的索引。
       int i2 = i2_off + k;
+      // load_idx 是从输入张量读取数据的索引，write_idx 是在 warp 缓冲区写入数据的索引。
       int load_idx = i1*n2+i2;
       int write_idx = thr_load_row_off*row_stride+thr_load_col_off+k;
+      // 如果 i2 在有效范围内，则从输入张量加载数据，并进行必要的计算。
       if (i2<n2) {
         U c_h = static_cast<U>(input_or_output[load_idx]);
         U curr_dout = static_cast<U>(dout[load_idx]);
+        // 根据 rms_only 和 MemoryEfficient 的值，使用不同的公式计算梯度，并将结果存储在 warp 缓冲区中。
         if (!rms_only) {
           warp_buf1[write_idx] = curr_dout;
           if (MemoryEfficient) {
@@ -548,6 +591,7 @@ void cuLoadWriteStridedInputs(
           }
         }
       } else {
+        // 对于超出 n2 范围的索引，将相应的 warp 缓冲区位置设置为 0。
         if (!rms_only) {
           warp_buf1[write_idx] = U(0);
         }
@@ -555,6 +599,7 @@ void cuLoadWriteStridedInputs(
       }
     }
   } else {
+    // 对于超出 n1 范围的索引，也将相应的 warp 缓冲区位置设置为 0。
     for (int k = 0;  k < blockDim.y;  ++k) {
       int write_idx = thr_load_row_off*row_stride+thr_load_col_off+k;
       if (!rms_only) {
@@ -565,6 +610,8 @@ void cuLoadWriteStridedInputs(
   }
 }
 
+// 这段代码定义了一个名为 cuLoadAddStridedInputs 的 CUDA 设备函数模板，
+// 用于在计算LayerNorm梯度的过程中加载输入数据并将其累加到 warp 级别的缓冲区中。
 template<typename T, typename U, typename V, bool MemoryEfficient> __device__
 void cuLoadAddStridedInputs(
     const int i1_block,
@@ -586,15 +633,21 @@ void cuLoadAddStridedInputs(
     bool rms_only
     )
 {
+  // 计算 i1，表示当前处理的数据行。
   int i1 = i1_block+thr_load_row_off;
+  // 外层的 if 判断确保只处理有效范围内的数据。
   if (i1 < i1_end) {
     for (int k = 0;  k < blockDim.y;  ++k) {
+      // 计算列索引 i2 以及用于加载和写入数据的索引。
       int i2 = i2_off + k;
+      // load_idx 用于从输入数据中读取，而 write_idx 用于在 warp 级别的缓冲区中写入。
       int load_idx = i1*n2+i2;
       int write_idx = thr_load_row_off*row_stride+thr_load_col_off+k;
+      // 如果 i2 在有效范围内，则从输入中加载数据并进行计算。
       if (i2<n2) {
         U c_h = static_cast<U>(input_or_output[load_idx]);
         U curr_dout = static_cast<U>(dout[load_idx]);
+        // 根据 rms_only 和 MemoryEfficient 的值进行条件累加，涉及到 gamma, beta, mean, invvar 等参数的使用。
         if (!rms_only) {
           U curr_beta = static_cast<U>(beta[i2]);
           warp_buf1[write_idx] += curr_dout;
@@ -614,7 +667,6 @@ void cuLoadAddStridedInputs(
     }
   }
 }
-
 
 template<typename T, typename U, typename V, bool MemoryEfficient> __global__
 void cuComputePartGradGammaBeta(
@@ -977,14 +1029,18 @@ void HostApplyLayerNorm(
     const V* beta
     )
 {
+    // threads和blocks定义了CUDA内核的线程和块的维度。这里，每个线程块有32×4的线程，而块的数量由n1和GPU设备的最大网格大小限制决定。
     auto stream = at::cuda::getCurrentCUDAStream().stream();
     const dim3 threads(32,4,1);
     const uint64_t maxGridY = at::cuda::getCurrentDeviceProperties()->maxGridSize[1];
     const dim3 blocks(1, std::min((uint64_t)n1, maxGridY), 1);
+    // 这段代码计算内核函数需要多少共享内存。如果threads.y大于1，它会根据U类型的大小分配足够的内存。
     int nshared =
         threads.y > 1 ?
             threads.y*sizeof(U)+(threads.y/2)*sizeof(U) :
             0;
+    // 最后，函数使用cuApplyLayerNorm kernel来执行实际的LayerNorm操作。
+    // kernel函数的调用使用了之前计算的线程块和线程配置，以及共享内存大小和CUDA流。
     cuApplyLayerNorm<<<blocks, threads, nshared, stream>>>(
       output, mean, invvar, input, n1, n2, U(epsilon), gamma, beta);
 }
@@ -1006,11 +1062,12 @@ void cuda_layer_norm(
     double epsilon)
 {
     using namespace at;
+    // 是一个宏，用于处理不同的数据类型（如double、float、half和bfloat）。
     DISPATCH_DOUBLE_FLOAT_HALF_AND_BFLOAT_INOUT_TYPES(
         input->scalar_type(), output->scalar_type(), "layer_norm_cuda_kernel",
         using accscalar_t = at::acc_type<scalar_t_in, true>;
         HostApplyLayerNorm<scalar_t_in, accscalar_t, scalar_t_out>(
-          output->DATA_PTR<scalar_t_out>(),
+          output->DATA_PTR<scalar_t_out>(), // 函数使用了PyTorch张量的数据指针（DATA_PTR）来获取底层数据的直接访问。
               mean->DATA_PTR<accscalar_t>(),
           invvar->DATA_PTR<accscalar_t>(),
           input->DATA_PTR<scalar_t_in>(),
@@ -1021,7 +1078,11 @@ void cuda_layer_norm(
       )
 }
 
-
+// 这是一个模板函数，支持不同的数据类型：T（输入数据类型）、
+// U（通常用于中间计算的数据类型，默认为float）、V（输出数据类型，默认与T相同）。
+// 参数包括输出梯度（dout）、均值（mean）、方差倒数（invvar）、输入或输出的PyTorch张量（input_or_output）、
+// 两个维度参数（n1、n2）、gamma和beta参数、用于数值稳定的epsilon、输入梯度（grad_input）、
+// gamma梯度（grad_gamma）和beta梯度（grad_beta）、以及一个指示是否优化内存使用的布尔值（memory_efficient）。
 template<typename T, typename U=float, typename V=T>
 void HostLayerNormGradient(
     const V* dout,
@@ -1039,24 +1100,38 @@ void HostLayerNormGradient(
     bool memory_efficient
     )
 {
+    // 获取当前CUDA流以用于后续的CUDA内核调用。
     auto stream = at::cuda::getCurrentCUDAStream().stream();
 
+    // 如果gamma和beta不为NULL，函数会计算它们的梯度。
+    // 这涉及两个CUDA内核的调用：cuComputePartGradGammaBeta和cuComputeGradGammaBeta。
     if (gamma != NULL && beta != NULL) {
       // compute grad_gamma(j) and grad_beta(j)
+      // part_size是分块计算梯度时的部分大小。
       const int part_size = 16;
+      // threads2定义了每个CUDA线程块中的线程数量（32×4×1）。
       const dim3 threads2(32,4,1);
+      // blocks2定义了CUDA网格中的块数量，其中，n2维度被分成多个块，以确保每个块可以处理n2中的一部分。
       const dim3 blocks2((n2+threads2.x-1)/threads2.x,part_size,1);
+      // 这部分代码计算用于CUDA内核的共享内存大小。nshared2_a和nshared2_b是基于线程和块维度的两种不同共享内存大小估算。
       const int nshared2_a = 2 * sizeof(U) * threads2.y * threads2.y * (threads2.x + 1);
       const int nshared2_b = threads2.x * threads2.y * sizeof(U);
+      // 最终选择较大的一个估算值作为实际的共享内存大小（nshared2）。
       const int nshared2 = nshared2_a > nshared2_b ? nshared2_a : nshared2_b;
       // note (mkozuki): I can hard code part_grad_gamma's dtype as float given that
       // the `cuda_layer_norm_gradient` doesn't support double.
+      // 根据输入或输出张量的数据类型决定局部梯度张量part_grad_gamma和part_grad_beta的数据类型。
+      // 如果输入或输出是半精度浮点数（Half）或BFloat16，则使用单精度浮点数（Float）；否则，使用输入或输出的相同数据类型。
       const auto part_grad_dtype =
         (input_or_output->scalar_type() == at::ScalarType::Half || input_or_output->scalar_type() == at::ScalarType::BFloat16) ?
         at::ScalarType::Float :
         input_or_output->scalar_type();
+      // 创建两个新的PyTorch张量part_grad_gamma和part_grad_beta，用于存储gamma和beta的局部梯度计算结果。
       at::Tensor part_grad_gamma = at::empty({part_size,n2}, input_or_output->options().dtype(part_grad_dtype));
       at::Tensor part_grad_beta = at::empty_like(part_grad_gamma);
+      // 使用BOOL_SWITCH宏处理memory_efficient参数，以决定是否使用内存高效版本的CUDA内核。
+      // 调用cuComputePartGradGammaBeta内核计算gamma和beta的梯度。
+      // 这个内核函数接收必要的输入参数，并将梯度结果写入part_grad_gamma和part_grad_beta张量。
       BOOL_SWITCH(memory_efficient, MemoryEfficient, [&]{
         auto kernel = &cuComputePartGradGammaBeta<T, U, V, MemoryEfficient>;
         kernel<<<blocks2, threads2, nshared2, stream>>>(
@@ -1074,9 +1149,14 @@ void HostLayerNormGradient(
                         false);
       });
 
+      // 定义了每个CUDA线程块中的线程数量（32×8×1）。
       const dim3 threads3(32,8,1);
+      // 定义了CUDA网格中的块数量。在这里，n2维度被分成多个块，每个块的大小由threads2.x（之前定义的线程数量）确定。
       const dim3 blocks3((n2+threads2.x-1)/threads2.x,1,1);
+      // 这行代码计算了cuComputeGradGammaBeta内核所需的共享内存大小。它基于threads3线程块的维度和数据类型U的大小。
       const int nshared3 = threads3.x * threads3.y * sizeof(U);
+      // kernel 接收局部梯度张量（part_grad_gamma和part_grad_beta）、块大小（part_size）、
+      // 维度参数（n1和n2）和指向梯度输出的指针（grad_gamma和grad_beta）。
       cuComputeGradGammaBeta<<<blocks3, threads3, nshared3, stream>>>(
                       part_grad_gamma.DATA_PTR<U>(),
                       part_grad_beta.DATA_PTR<U>(),
@@ -1088,13 +1168,20 @@ void HostLayerNormGradient(
     }
 
     // compute grad_input
+    // 这行代码获取当前CUDA设备支持的最大网格尺寸（在Y维度上）。这是为了确保线程块的配置不会超过GPU的硬件限制。
     const uint64_t maxGridY = at::cuda::getCurrentDeviceProperties()->maxGridSize[1];
+    // blocks1定义了CUDA网格中的块数量。在这里，Y维度的大小被限制为n1和maxGridY中的较小者，确保不超过GPU的最大网格尺寸限制。
     const dim3 blocks1(1, std::min((uint64_t)n1, maxGridY), 1);
+    // threads1定义了每个CUDA线程块中的线程数量（32×4×1）。
     const dim3 threads1(32,4,1);
+    // 这行代码根据线程块的维度计算所需的共享内存大小。如果threads1.y大于1，则根据U类型的大小和线程块的维度来分配足够的共享内存。
     int nshared =
             threads1.y > 1 ?
             threads1.y*threads1.x*sizeof(U) :
             0;
+    // cuComputeGradInput内核被调用来计算输入张量的梯度。这个内核接收必要的输入参数，并将梯度结果写入grad_input
+    // 参数包括输出梯度（dout）、输入或输出的数据指针、维度参数（n1和n2）、均值（mean）、方差倒数（invvar）、
+    // 用于数值稳定性的epsilon、gamma和beta参数。
     BOOL_SWITCH(memory_efficient, MemoryEfficient, [&] {
       auto kernel = cuComputeGradInput<T, U, V, MemoryEfficient>;
       kernel<<<blocks1, threads1, nshared, stream>>>(
